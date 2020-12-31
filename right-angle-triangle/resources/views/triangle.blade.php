62bbd218724edc5645cad141aba3f0f1adc1045a
nwaito <nathanjwaito@gmail.com>
2020-12-31 11:34:59 -0500

    feat: adds bootstrap scaffolding to project

--- right-angle-triangle/resources/views/triangle.blade.php
+++ right-angle-triangle/resources/views/triangle.blade.php
@@ -2,48 +2,57 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <title>Right Angle Triangle</title>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-        <h2>Right Angle Triangles.</h2>
-        <form method="POST" action="">
-            @csrf
-            <label for="a">a</label>
-            <input id="a" name="a" type="text" class="@error('a') is-invalid @enderror" value="{{ $a ?? '' }}">
-            <label for="b">b</label>
-            <input id="b" name="b" type="text" class="@error('b') is-invalid @enderror" value="{{ $b ?? '' }}">
-            <label for="c">c</label>
-            <input id="c" name="c" type="text" class="@error('c') is-invalid @enderror" value="{{ $c ?? '' }}">
-            <label for="theta">theta</label>
-            <input id="theta" name="theta" type="text" class="@error('theta') is-invalid @enderror"
-                   value="{{ $theta ?? '' }}">
+        <div class="container">
 
-            <input id="submit" type="submit">
+            <h2>Right Angle Triangles.</h2>
+            <form method="POST" action="">
+                @csrf
+                <label for="a">a</label>
+                <input id="a" name="a" type="text" class="@error('a') is-invalid @enderror" value="{{ $a ?? '' }}">
+                <label for="b">b</label>
+                <input id="b" name="b" type="text" class="@error('b') is-invalid @enderror" value="{{ $b ?? '' }}">
+                <label for="c">c</label>
+                <input id="c" name="c" type="text" class="@error('c') is-invalid @enderror" value="{{ $c ?? '' }}">
+                <label for="theta">theta</label>
+                <input id="theta" name="theta" type="text" class="@error('theta') is-invalid @enderror"
+                       value="{{ $theta ?? '' }}">
 
-            @if( $errors->hasAny ( 'a', 'b', 'c', 'theta' ) )
-                <div class="alert alert-danger">{{$errors}}</div>
-            @endif
-        </form>
+                <input id="submit" type="submit">
 
-        <table id="recent-results">
-            <tr>
-                <th>uniqueId</th>
-                <th>theta</th>
-                <th>a</th>
-                <th>b</th>
-                <th>c</th>
-                <th>date</th>
-                <th>Values modified</th>
-            </tr>
-            @foreach( $triangles as $triangle )
+                @if( $errors->hasAny ( 'a', 'b', 'c', 'theta' ) )
+                    <div class="alert alert-danger">{{$errors}}</div>
+                @endif
+            </form>
+
+            <table id="recent-results">
                 <tr>
-                    <td>{{$triangle->id}}</td>
-                    <td>{{$triangle->theta}}</td>
-                    <td>{{$triangle->a}}</td>
-                    <td>{{$triangle->b}}</td>
-                    <td>{{$triangle->c}}</td>
-                    <td>{{$triangle->created_at}}</td>
+                    <th>uniqueId</th>
+                    <th>theta</th>
+                    <th>a</th>
+                    <th>b</th>
+                    <th>c</th>
+                    <th>date</th>
+                    <th>Values modified</th>
                 </tr>
-            @endforeach
-        </table>
+                @foreach( $triangles as $triangle )
+                    <tr>
+                        <td>{{$triangle->id}}</td>
+                        <td>{{$triangle->theta}}</td>
+                        <td>{{$triangle->a}}</td>
+                        <td>{{$triangle->b}}</td>
+                        <td>{{$triangle->c}}</td>
+                        <td>{{$triangle->created_at}}</td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
     </body>
 </html>
